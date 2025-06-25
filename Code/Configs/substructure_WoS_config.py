b99c1e22a7a2902d1490eac597991563ca1dcb21
l.obrien.smn@gmail.com <l.obrien.smn@gmail.com>
2025-06-25 13:53:07 -0700

    Remove unnecessary imports and path definitions

--- Code/Configs/substructure_WoS_config.py
+++ Code/Configs/substructure_WoS_config.py
@@ -1,21 +1,12 @@
 # Includes a PEMD deflector with external shear, and Sersic sources. 
 
 import numpy as np
-from scipy.stats import norm, truncnorm, uniform
-import sys
-import paltas.Sampling.distributions as dist
-
 from paltas.MainDeflector.simple_deflectors import PEMDShear
 from paltas.Sources.sersic import SingleSersicSource
 from paltas.PointSource.single_point_source import SinglePointSource
-from paltas.Substructure.subhalos_dg19 import SubhalosDG19
 from lenstronomy.Util import kernel_util
-from lenstronomy.Util.param_util import phi_q2_ellipticity
-import os
-import paltas
 
-file = '/Users/Logan/Documents/SJSU/HEP_Research/ComplexLA/Data-Tables/substructure_parameters_catalog.csv'
-path = os.path.abspath(os.path.join('..'))+'/'
+file = '../../Data-Tables/substructure_parameters_catalog.csv'
 
 # Define the numerics kwargs.
 kwargs_numerics = {'supersampling_factor':1}
@@ -34,7 +25,7 @@ catalog = True
 index = None
 
 def draw_psf_kernel():
-    weighted_sum = np.load(path+'Files/weighted_sum.npy')
+    weighted_sum = np.load('../Files/weighted_sum.npy')
     return kernel_util.degrade_kernel(weighted_sum,4)
 
 # you're appending root path to each file path you provide!
