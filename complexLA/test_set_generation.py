632e4b3d007f49d4d077e0bf585def1bb5c31aa8
l.obrien.smn@gmail.com <l.obrien.smn@gmail.com>
2026-07-13 12:34:52 -0700

    Add function to display residual images. Add documentation.

--- complexLA/test_set_generation.py
+++ complexLA/test_set_generation.py
@@ -3,6 +3,7 @@ This module contains functions to call paltas scirpts that will generate test se
 Images of the lenses are stored in user defined locations.
 """
 from astropy.visualization import AsinhStretch, ImageNormalize
+import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -24,6 +25,9 @@ class TestSetGeneration():
         self.sets = sets
 
     def generate_images(self):
+        """
+        Generates a test set of a user defined size of simulated lens systems.
+        """
         for set in self.sets:
             config_file = '../complexLA/Configs/'+set+'_config.py'
             image_path = '../images_for_network/'+set
@@ -40,6 +44,14 @@ class TestSetGeneration():
             #os.system(f'python /Users/Logan/AppData/Local/Programs/Python/Python312/Lib/site-packages/paltas/paltas/generate.py {config_file} {image_path} --n {self.sample_num} --tf_record')
    
     def display_sample(self,save_fig=False):
+        """
+        Displays the test set images in a grid.
+
+        Parameters
+        ----------
+        save_fig : bool
+            Determines if the test set images should be saved to the local device
+        """
 
         #Initializing array that will store the images from the image path
         for set in self.sets:
@@ -66,3 +78,48 @@ class TestSetGeneration():
             plt.show()
             if save_fig==True:
                 fig.savefig('../docs/figures/'+set+'_sample.png',bbox_inches='tight')
+
+    def residuals(self,sample_num,set,save_fig=False):
+        """
+        Displays the residuals between the complex mass model test sets and the simple main deflector test set.
+
+        Parameters
+        ----------
+        sample_num : int
+            Number of simulated lenses in the test set
+        set : str
+            Test set to be compared with the simple main deflector test set
+        save_fig : bool
+            Determines if the test set images should be saved to the local device
+        """
+        #Initializing array that will store the images from the image path
+        im = []
+        im_md = []
+        image_path = '../images_for_network/'+set
+        image_path_md = '../images_for_network/main_deflector'
+
+        #Populating image array
+        for file in os.listdir(image_path):
+            if file.endswith('.npy'):
+                im.append(file)
+
+        for file in os.listdir(image_path_md):
+            if file.endswith('.npy'):
+                im_md.append(file)
+
+        #Setting the norm and color scale
+        resid_norm =mcolors.TwoSlopeNorm(vmin=-0.025,vcenter=0,vmax=0.025)
+
+        #Defining details of the grid
+        fig,axs = plt.subplots(10,10,figsize=(10,10))
+        n_cols = 10
+
+        for i in range(0,sample_num-1):
+            imris = axs[i//n_cols,i%n_cols].imshow(np.load(image_path+im[i])-np.load(image_path_md+im_md[i]), norm=resid_norm,cmap='bwr')
+            axs[i//n_cols,i%n_cols].set_xticks([])
+            axs[i//n_cols,i%n_cols].set_yticks([])
+
+        fig.colorbar(imris, ax=axs)
+        plt.show()
+        if save_fig==True:
+            fig.savefig('../docs/figures/resid_'+set+'.png',bbox_inches='tight')
